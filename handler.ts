36d8a8586de4849dc2ad15b33d9b8911aee4f443
Raviteja Lingineni <rlingineni@live.com>
2021-08-16 15:08:01 -0500

    Update handler.ts

--- handler.ts
+++ handler.ts
@@ -5,20 +5,25 @@ const Koa = require("koa");
 const Router = require("@koa/router");
 const koaBody = require("koa-body");
 
+import PetsRouter from './routes/pets';
+
 const app = new Koa();
 app.use(koaBody());
 
-// import routes from other files as necessary
 const router = new Router();
 app.use(router.routes());
 
+// import router from other files
+app.use(PetsRouter.routes());
+app.use(PetsRouter.allowedMethods());
+
+
+// get the file as needed
 router.get("/", (ctx, next) => {
   ctx.status = 200;
   ctx.body = {msg:"You're Serverless API is ready"};
   return;
 });
-
-
 
 export const main: APIGatewayProxyHandler = serverless(app, {
   binary: ["image/png"],
