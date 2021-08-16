1feae9c97dfa58fd597cbc293a800c6aaa9e92b3
Raviteja Lingineni <rlingineni@live.com>
2021-08-16 14:38:52 -0500

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,26 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+const serverless = require("serverless-http");
+
+const Koa = require("koa");
+const Router = require("@koa/router");
+const koaBody = require("koa-body");
+
+const app = new Koa();
+app.use(koaBody());
+
+// import routes from other files as necessary
+
+
+router.get("/", (ctx, next) => {
+  ctx.status = 200;
+  ctx.body = {msg:"You're Serverless API is ready"};
+  return;
+});
+
+
+
+export const main: APIGatewayProxyHandler = serverless(app, {
+  binary: ["image/png"],
+});
+
+export { app };
